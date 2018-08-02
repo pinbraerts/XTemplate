63e34e65c8bb7d2146126e1d44bf5d8c258cb193
pinbraerts <shirshov.dima@gmail.com>
2018-08-02 15:12:54 +0300

    cself -> self

--- core/includes.hpp
+++ core/includes.hpp
@@ -11,4 +11,17 @@
 
 template<class T, size_t N> constexpr size_t length(T(&)[N]) { return N; }
 
+template<class R> R IfCV_cast(auto& t) {
+    return static_cast<R>(t);
+}
+template<class R> const std::remove_reference_t<R>& IfCV_cast(const auto& t) {
+    return static_cast<const std::remove_reference_t<R>&>(t);
+}
+template<class R> volatile R IfCV_cast(volatile auto& t) {
+    return static_cast<volatile R>(t);
+}
+template<class R> const volatile R IfCV_cast(const volatile auto& t) {
+    return static_cast<const volatile R>(t);
+}
+
 #endif // !INCLUDES_H
